9b047617d12569d996fd321044834f5c35327b0a
Sergio <rodriguezmsergio@gmail.com>
2018-09-11 00:24:31 -0300

    version 2.2... probar empezar por atras

--- src/Secuencia.java
+++ src/Secuencia.java
@@ -6,12 +6,13 @@ public class Secuencia {
 	
 	static boolean secuenciaCiclica(int[] s) {
 		
-		int i=0, c=0, n=s.length;
+		int i=1, c=0, n=s.length;
 				
-		for(; i<n-1;)
-			c += s[i]>s[++i]?1:0;
-
-		return c==1 && s[n-1]<s[0] || n<2?1>0:0>1;
+		for(int x:s) 
+			c += x>s[i++%n]?1:0;
+		
+		return c<2 && s[n-1]<s[0] || n<2 ?1>0:0>1;
 		
 	}
+	
 }
